f07adf6b2130b710a78c4cb76c0afa38c6a0be14
Dmytro Davidenko <dima080885@gmail.com>
2022-12-16 22:07:46 +0200

    bgfx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Section, Filter, Contacts, NewContactForm } from './';
 import styled from '@emotion/styled';

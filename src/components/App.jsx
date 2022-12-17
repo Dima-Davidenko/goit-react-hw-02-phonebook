be964d9df440d60efd793e60179a8ca3355dbae3
Dmytro Davidenko <dima080885@gmail.com>
2022-12-17 17:37:04 +0200

    bug fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,35 +13,43 @@ const Container = styled.div`
 export class App extends Component {
   state = {
     contacts: [...phonebook],
-    filteredContacts: [],
     filter: '',
   };
 
   addContact = ({ name, number }) => {
-    const newContact = {
-      name,
-      number,
-      id: nanoid(),
-    };
-    this.setState(prevState => ({ contacts: [...prevState.contacts, newContact] }));
+    if (this.state.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+      alert('Контакт з таким іменем вже існує');
+    } else {
+      this.setState(prevState => ({
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: nanoid(),
+            name,
+            number,
+          },
+        ],
+      }));
+    }
   };
 
-  filterContacts = filter => {
-    const { contacts } = this.state;
+  setFilter = filter => {
     this.setState({
-      filteredContacts: contacts.filter(({ name }) => name.toLowerCase().includes(filter)),
-      filter,
+      filter: filter.toLowerCase().trim(),
     });
   };
 
+  filterContacts = () => {
+    const { contacts } = this.state;
+    return contacts.filter(({ name }) => name.toLowerCase().includes(this.state.filter));
+  };
+
   removeContact = idToRemove => {
-    this.setState(({ contacts, filteredContacts }) => ({
+    this.setState(({ contacts }) => ({
       contacts: contacts.filter(({ id }) => id !== idToRemove),
-      filteredContacts: filteredContacts.filter(({ id }) => id !== idToRemove),
     }));
   };
   render() {
-    const { contacts, filteredContacts, filter } = this.state;
     return (
       <Container>
         <Typography variant="h2" sx={{ mb: 10 }}>
@@ -51,11 +59,8 @@ export class App extends Component {
           <NewContactForm addContact={this.addContact} />
         </Section>
         <Section title="Ваші контакти" variant="h3">
-          <Contacts
-            contactsToShow={filteredContacts.length || filter ? filteredContacts : contacts}
-            removeContact={this.removeContact}
-          >
-            <Filter filterContacts={this.filterContacts} />
+          <Contacts contactsToShow={this.filterContacts()} removeContact={this.removeContact}>
+            <Filter setFilter={this.setFilter} />
           </Contacts>
         </Section>
       </Container>
